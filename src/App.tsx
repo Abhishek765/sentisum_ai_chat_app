ae7086b27630fff57c198602bafeeb2375b34476
Abhishek765 <vishwakarmaabhishek31@gmail.com>
2025-01-30 21:04:25 +0530

    feat: layout code + toggle theme dispatcher

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
-import { Provider } from "react-redux";
+import { Provider, useDispatch } from "react-redux";
 import { store } from "./store";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ChatInterface, SharedConversation } from "./components";
+import { AppBar, Toolbar, Typography, IconButton } from "@mui/material";
+import { Brightness7 } from "@mui/icons-material";
+import { toggleTheme } from "./store/themeSlice";
 
 function AppContent() {
+  const dispatch = useDispatch();
+
   return (
-    <Routes>
-      <Route path="/" element={<ChatInterface />} />
-      <Route path="/share/:id" element={<SharedConversation />} />
-    </Routes>
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="h1" sx={{ flexGrow: 1 }}>
+            SentiSum Ai Chat Interface
+          </Typography>
+          <IconButton color="inherit" onClick={() => dispatch(toggleTheme())}>
+            <Brightness7 />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <Routes>
+        <Route path="/" element={<ChatInterface />} />
+        <Route path="/share/:id" element={<SharedConversation />} />
+      </Routes>
+    </>
   );
 }
 
